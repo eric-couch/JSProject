9eff21c184b8e9718b316203d1534ed771dae4ee
Eric Couch <ericcouch@yahoo.com>
2024-11-13 22:58:52 -0600

    initial

---
+++ js/favoritemovies.js
@@ -0,0 +1,47 @@
+const OMDBAPIUrl = "https://www.omdbapi.com/?apikey=";
+const OMDBAPIKey = "86c39163";
+let SearchTitle = "";
+let MoviePoster = "https://img.omdbapi.com/?apikey=86c39163&i="
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    let jsonString = localStorage.getItem('favMovieList');
+    console.log(jsonString);
+    if (jsonString != null) {
+        let favoriteMovies = JSON.parse(jsonString);
+        favoriteMovies.forEach ((movie) => {
+            (async () => {
+                const movieDetails = await getMovie(movie.imdbID);
+                BuildMovieCard(movieDetails);
+            })();
+        });
+    }
+});
+
+function BuildMovieCard(movie) {
+    const { Title, Year, imdbID, Genre, Plot, Ratings } = movie;
+    const movieCard = document.getElementById('movieCard');
+    let movieCardClone = movieCard.cloneNode(true);
+    movieCardClone.querySelector('#moviePoster').src = MoviePoster + imdbID;
+    let cardBody = movieCardClone.querySelector('#cardBody');
+    cardBody.firstElementChild.innerText = `${Title} (${Year})`;
+    let genre = cardBody.querySelector('#genre');
+    genre.innerText = `Genre: ${Genre}`;
+    genre.style.fontWeight = 'bold';
+    let plot = cardBody.querySelector('#plot');
+    plot.innerText = `Plot: ${Plot}`;
+    let favButton = movieCardClone.querySelector('#favbtn');
+    if (favButton != null) {
+        movieCardClone.querySelector('#favbtn').addEventListener('click', (event) => {
+            UpdateFavoriteMovies(movie);
+        });
+    }
+    favMovieList.firstElementChild.appendChild(movieCardClone);
+    movieCardClone.style.display = 'inline';
+}
+
+const getMovie = async (imdbId) => {
+    const movieDetailsURL = `${OMDBAPIUrl}${OMDBAPIKey}&i=${imdbId}`;
+    const response = await fetch(movieDetailsURL);
+    return await response.json();
+  };
---
+++ js/moviesearch.js
@@ -0,0 +1,57 @@
+const OMDBAPIUrl = "https://www.omdbapi.com/?apikey=";
+const OMDBAPIKey = "86c39163";
+let SearchTitle = "";
+let MoviePoster = "https://img.omdbapi.com/?apikey=86c39163&i="
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    const button = document.getElementById('searchbtn');
+    button.addEventListener('click', (event) => {
+        console.log('button clicked');
+        (async () => {
+            const movies = await getMovies();
+            console.log(movies);
+            const favMovieList = document.getElementById('favMovieList');
+            favMovieList.firstElementChild.innerHTML = '';
+            movies.Search.forEach((movie) => {
+                console.log(movie.Title);
+                BuildMovieCard(movie);
+            });
+        })();
+    });
+});
+
+function BuildMovieCard(movie) {
+    const { Title, Year, imdbID, Type, Poster } = movie;
+    const movieCard = document.getElementById('movieCard');
+    let movieCardClone = movieCard.cloneNode(true);
+    movieCardClone.querySelector('#moviePoster').src = MoviePoster + imdbID;
+    movieCardClone.querySelector('#cardBody').firstElementChild.innerText = `${Title} (${Year})`;
+    let favButton = movieCardClone.querySelector('#favbtn');
+    if (favButton != null) {
+        movieCardClone.querySelector('#favbtn').addEventListener('click', (event) => {
+            UpdateFavoriteMovies(movie);
+        });
+    }
+    favMovieList.firstElementChild.appendChild(movieCardClone);
+    movieCardClone.style.display = 'inline';
+}
+
+function UpdateFavoriteMovies(movie) {
+    let jsonString = localStorage.getItem('favMovieList');
+    console.log(jsonString);
+        if (jsonString === null) {
+            jsonString = '[]';
+        }
+        let favoriteMovies = JSON.parse(jsonString);
+        favoriteMovies.push(movie);
+        console.log(favoriteMovies);
+        localStorage.setItem('favMovieList', JSON.stringify(favoriteMovies));
+}
+
+const getMovies = async () => {
+    SearchTitle = document.getElementById('searchTerm').value;
+    const searchURL = `${OMDBAPIUrl}${OMDBAPIKey}&s=${SearchTitle}`;
+    const response = await fetch(searchURL);
+    return await response.json();
+  };
